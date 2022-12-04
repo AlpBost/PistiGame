e48b215e8967d87232bf6e3273b6bfe14d1df30e
AlpBost <alpbost1@gmail.com>
2022-12-04 13:53:54 +0300

    Info about the deck
    The first draft of my project

--- MyProject.java
+++ MyProject.java
@@ -1,5 +1,41 @@
-public class MyProject {
+import java.util.Scanner;
+import java.util.Random;
+
+public class My Project {
+	Random rd = new Random(System.currentTimeMillis());
+	int [] deck = new int [52];
+	int [] ♦ = new int [13];
+	int [] ♥ = new int [13];
+	int [] ♣ = new int [13];
+	int [] ♠ = new int [13];
+	// started deck
+	for (int i= 2; i<11; i++) {
+		♦ [i] = i ;
+		♥ [i] = i ;
+		♣ [i] = i ;
+		♠ [i] = i ;
+	}// gaven point
+	♦ [1] = A;
+	♥ [1] = A;
+	♣ [1] = A;
+	♠ [1] = A;
+
+	♦ [11] = J;
+	♥ [11] = J;
+	♣ [11] = J;
+	♠ [11] = J;
+
+	♦ [12] = Q;
+	♥ [12] = Q;
+	♣ [12] = Q;
+	♠ [12] = Q;
+
+	♦ [13] = K;
+	♥ [13] = K;
+	♣ [13] = K;
+	♠ [13] = K;
+
+	//dont forget to shuffle 
+
 	
-	
-	
-}
+
